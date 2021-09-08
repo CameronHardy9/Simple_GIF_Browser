2399ab0f669fe22a91330a7d1e057ee5196d5971
Cameron Hardy <camhardy99@gmail.com>
2021-09-08 15:07:01 -0500

    minimal exercise done - will add extra functionality

--- app.js
+++ app.js
@@ -2,3 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const apiKey = "uX6xKR7FNjOPuvsJ6dShPV1o3crQBbP7";
+const giphyURL = "http://api.giphy.com/v1/gifs/translate";
+const button = document.querySelector(".submit");
+const img = document.querySelector("#imageContainer > img");
+
+button.onclick = () => {
+    const search = document.querySelector("#input").value;
+    fetch(`${giphyURL}?api_key=${apiKey}&s=${search}`, {
+        mode: "cors"
+    })
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+            img.src = data.data.images.original.url;
+        })
+        .catch((err) => {
+            throw new Error("Something went wrong");
+        })
+}
