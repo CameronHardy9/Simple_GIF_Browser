2d106aaefbdc7e16a9373ebe38e91f1e7e1f0576
Cameron Hardy <camhardy99@gmail.com>
2021-09-08 16:00:59 -0500

    search bar placeholder uses giphy api for suggested input - assets added for giphy attribution

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-console.log("Hello World!\n==========\n");
+"use strict";
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
-
-const apiKey = "uX6xKR7FNjOPuvsJ6dShPV1o3crQBbP7";
-const giphyURL = "http://api.giphy.com/v1/gifs/translate";
+const _apiKey = "uX6xKR7FNjOPuvsJ6dShPV1o3crQBbP7";
+const giphyURL = "https://api.giphy.com/v1/gifs/translate";
 const button = document.querySelector(".submit");
-const img = document.querySelector("#imageContainer > img");
+const img = document.querySelector(".gif > img");
+const search = document.querySelector("#input");
 
+
+//pulls popular searches from giphy api and adds them as search placeholder
+window.onload = searchSuggestion();
+
+
+//checks for user input - then searches giphy database using input
 button.onclick = () => {
-    const search = document.querySelector("#input").value;
-    fetch(`${giphyURL}?api_key=${apiKey}&s=${search}`, {
+    if(!search.value) {
+
+    } else {
+        fetch(`${giphyURL}?api_key=${_apiKey}&s=${search.value}`, {
+            mode: "cors"
+        })
+            .then((data) => {
+                return data.json();
+            })
+            .then((data) => {
+                img.src = data.data.images.original.url;
+            })
+            .catch((err) => {
+                throw new Error("Something went wrong");
+            })
+    }
+    
+}
+
+
+function searchSuggestion () {
+    fetch((`https://api.giphy.com/v1/trending/searches?api_key=${_apiKey}`), {
         mode: "cors"
     })
         .then((data) => {
             return data.json();
         })
         .then((data) => {
-            img.src = data.data.images.original.url;
-        })
-        .catch((err) => {
-            throw new Error("Something went wrong");
+            search.placeholder = `${data.data[Math.floor(Math.random()*data.data.length)]}...`;
         })
 }
