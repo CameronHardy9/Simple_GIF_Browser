58cb3cd7332d33dbd8c3e12c79020625b14550d1
Cameron Hardy <camhardy99@gmail.com>
2021-09-14 11:54:15 -0500

    cleaned up code a bit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const _shrtcoURL = "https://api.shrtco.de/v2/shorten?url="
+const _shrtcoURL = "https://api.shrtco.de/v2/shorten?url=";
 const _giphyApiKey = "uX6xKR7FNjOPuvsJ6dShPV1o3crQBbP7";
 const _giphyURL = "https://api.giphy.com/v1/gifs/translate";
 const button = document.querySelector(".submit");
@@ -21,10 +21,10 @@ window.onload = searchSuggestion();
 
 //Event listener for pressing enter to submit input
 search.addEventListener("keyup", (key) => {
-    if (key.key === "Enter"){
+    if (key.key === "Enter") {
         button.click();
     }
-})
+});
 
 //Event listener for navigating forward and back through gifs
 window.addEventListener("keyup", (key) => {
@@ -33,27 +33,26 @@ window.addEventListener("keyup", (key) => {
     } else if (key.key === "ArrowRight") {
         next.onclick();
     }
-})
+});
 
 //Checks for user input - then searches giphy database using input
 button.onclick = () => {
-    if(!search.value) {
+    if (!search.value) {
         //TODO: Insert error for no text
     } else {
         newSearch();
     }
-    
-}
+};
 
 //Clipboard API
 clip.onclick = () => {
     navigator.clipboard.writeText(viewHistory[viewPosition].clipLink);
-}
+};
 
 //Cycles to next random gif under same search param
 next.onclick = () => {
     if (viewPosition == viewHistory.length - 1) {
-        if(!search.value) {
+        if (!search.value) {
             //TODO: Insert error for no text
         } else {
             newSearch();
@@ -61,7 +60,7 @@ next.onclick = () => {
     } else {
         nextItem();
     }
-}
+};
 
 //
 previous.onclick = () => {
@@ -70,88 +69,89 @@ previous.onclick = () => {
     } else {
         previousItem();
     }
-}
-
+};
 
 // -- FUNCTIONS --
 
 //Performs fetch of gif from giphy based on search param, then stores gif data in array for future reference without additional api calls
 function newSearch() {
     fetch(`${_giphyURL}?api_key=${_giphyApiKey}&s=${search.value}`, {
-        mode: "cors"
-    })
-    .then((data) => {
-        return data.json();
-    })
-    .then((result) => {
-        viewPosition = viewHistory.length;
-        viewHistory.push({
-            image: result.data.images.original.url,
-            imageLink: result.data.url,
-            clipLink: shrinkURL(result.data.images.original.url)
-        });
-        img.src = viewHistory[viewPosition].image;
-        imgURL.href = viewHistory[viewPosition].imageLink;
-        shortURL.value = viewHistory[viewPosition].clipLink;
-        next.hidden = false;
-        previous.hidden = false;
-        imgContainer.hidden = false;
-        gifGen.classList = "reveal-gifGen";
-        imgURL.hidden = false;
-    })
-    .catch((err) => {
-        if(img.src == undefined){
-            throw new Error("GIF is not available. Please try again.");
-        } else {
-            throw new Error(err);
-        }
-    })
-}
-
-function nextItem() {
-    viewPosition ++;
-    const nextItem = viewHistory[viewPosition];
-    img.src = nextItem.image;
-    imgURL.href = nextItem.imageLink;
-    shortURL.value = nextItem.clipLink
-    
-}
-
-function previousItem() {
-    viewPosition --;
-    const previousItem = viewHistory[viewPosition];
-    img.src = previousItem.image;
-    imgURL.href = previousItem.imageLink;
-    shortURL.value = previousItem.clipLink
-}
-
-//URL shortener for clip feature
-function shrinkURL(url) {
-    let output;
-    fetch(`${_shrtcoURL}${url}`, {
-        mode: "cors"
-    })
-    .then((data) => {
-        return data.json();
-    })
-    .then((result) => {
-        viewHistory[viewPosition].clipLink = result.result.short_link;
-    })
-    .catch((err) => {
-        throw new Error(err);
-    })
-    return output;
-}
-
-//API call to generate search suggestions based on most searched items from giphy
-function searchSuggestion () {
-    fetch((`https://api.giphy.com/v1/trending/searches?api_key=${_giphyApiKey}`), {
-        mode: "cors"
+        mode: "cors",
     })
         .then((data) => {
             return data.json();
         })
-        .then((data) => {
-            search.placeholder = `${data.data[Math.floor(Math.random()*data.data.length)]}...`;
+        .then((result) => {
+            viewPosition = viewHistory.length;
+            viewHistory.push({
+                image: result.data.images.original.url,
+                imageLink: result.data.url,
+                clipLink: shrinkURL(result.data.images.original.url),
+            });
+            img.src = viewHistory[viewPosition].image;
+            imgURL.href = viewHistory[viewPosition].imageLink;
+            shortURL.value = viewHistory[viewPosition].clipLink;
+            next.hidden = false;
+            previous.hidden = false;
+            imgContainer.hidden = false;
+            gifGen.classList = "reveal-gifGen";
+            imgURL.hidden = false;
         })
+        .catch((err) => {
+            if (img.src == undefined) {
+                throw new Error("GIF is not available. Please try again.");
+            } else {
+                throw new Error(err);
+            }
+        });
+}
+
+function nextItem() {
+    viewPosition++;
+    const nextItem = viewHistory[viewPosition];
+    img.src = nextItem.image;
+    imgURL.href = nextItem.imageLink;
+    shortURL.value = nextItem.clipLink;
+}
+
+function previousItem() {
+    viewPosition--;
+    const previousItem = viewHistory[viewPosition];
+    img.src = previousItem.image;
+    imgURL.href = previousItem.imageLink;
+    shortURL.value = previousItem.clipLink;
+}
+
+//URL shortener for clip feature
+function shrinkURL(url) {
+    fetch(`${_shrtcoURL}${url}`, {
+        mode: "cors",
+    })
+        .then((data) => {
+            return data.json();
+        })
+        .then((result) => {
+            viewHistory[viewPosition].clipLink = result.result.short_link;
+        })
+        .catch((err) => {
+            throw new Error(err);
+        });
+}
+
+//API call to generate search suggestions based on most searched items from giphy
+function searchSuggestion() {
+    fetch(
+        `https://api.giphy.com/v1/trending/searches?api_key=${_giphyApiKey}`,
+        {
+            mode: "cors",
+        }
+    )
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+            search.placeholder = `${
+                data.data[Math.floor(Math.random() * data.data.length)]
+            }...`;
+        });
 }
